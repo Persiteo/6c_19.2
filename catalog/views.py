abf814c62c78b9fc1ace8a93d69547bcce836f27
Persiteo <Persiteo@users.noreply.github.com>
2023-08-24 22:55:15 +0300

    added app without post

--- catalog/views.py
+++ catalog/views.py
@@ -2,18 +2,22 @@ from django.shortcuts import render
 
 
 def index(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        message = request.POST.get('message')
-        print(f'You have new message from {name}({email}): {message}')
-    return render(request, 'main/index.html')
+    # if request.method == 'POST':
+    #     name = request.POST.get('name')
+    #     email = request.POST.get('email')
+    #     message = request.POST.get('message')
+    #     print(f'You have new message from {name}({email}): {message}')
+    return render(request, 'catalog/index.html')
 
 
-def contact(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        message = request.POST.get('message')
-        print(f'You another one new message from {name}({email}): {message}')
-    return render(request, 'main/contacts.html')
+def contacts(request):
+    # if request.method == 'POST':
+    #     name = request.POST.get('name')
+    #     email = request.POST.get('email')
+    #     message = request.POST.get('message')
+    #     print(f'You another one new message from {name}({email}): {message}')
+    return render(request, 'catalog/contacts.html')
+
+
+def howtogo(request):
+    return render(request, 'catalog/howtogo.html')
